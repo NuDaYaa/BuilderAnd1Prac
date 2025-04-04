d10e2d45d9d392745e261261ef61e58be7e782e0
EPTABLYARTX4070SUPER <EPTABLYARTX4070SUPER@HOME-PC>
2025-04-04 06:38:45 +0300

    Добавьте файлы проекта.

---
+++ Matrica/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 5; 
+        int m = 6; 
+        double[,] matrix = GenerateRandomMatrix(n, m);
+        PrintMatrix(matrix);
+        FindNegativeColumns(matrix);
+    }
+
+    static double[,] GenerateRandomMatrix(int rows, int cols)
+    {
+        Random rand = new Random();
+        double[,] matrix = new double[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                matrix[i, j] = rand.NextDouble() * 20 - 10; 
+            }
+        }
+        return matrix;
+    }
+
+    static void PrintMatrix(double[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        Console.WriteLine("Сгенерированная матрица:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write($"{matrix[i, j],8:F2} ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+
+    static void FindNegativeColumns(double[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        Console.WriteLine("Столбцы, содержащие только отрицательные элементы:");
+        bool found = false;
+
+        for (int j = 0; j < cols; j++)
+        {
+            bool allNegative = true;
+
+            for (int i = 0; i < rows; i++)
+            {
+                if (matrix[i, j] >= 0)
+                {
+                    allNegative = false;
+                    break;
+                }
+            }
+
+            if (allNegative)
+            {
+                Console.WriteLine($"Столбец {j + 1}");
+                found = true;
+            }
+        }
+
+        if (!found)
+        {
+            Console.WriteLine("Нет таких столбцов.");
+        }
+    }
+}
---
+++ Matrix/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class FitnessProgram
+{
+    public List<string> Exercises { get; set; } = new List<string>();
+    public string DifficultyLevel { get; set; }
+
+    public void ShowProgram()
+    {
+        Console.WriteLine($"Программа тренировки ({DifficultyLevel} уровень):");
+        foreach (var exercise in Exercises)
+        {
+            Console.WriteLine("- " + exercise);
+        }
+        Console.WriteLine();
+    }
+}
+
+public interface IWorkoutBuilder
+{
+    void AddCardio();
+    void AddStrengthTraining();
+    void AddStretching();
+    void AddYoga();
+    FitnessProgram GetProgram();
+}
+
+public class BeginnerWorkoutBuilder : IWorkoutBuilder
+{
+    private readonly FitnessProgram _program = new FitnessProgram { DifficultyLevel = "Начальный" };
+
+    public void AddCardio()
+    {
+        _program.Exercises.Add("Легкий бег 5 минут");
+    }
+
+    public void AddStrengthTraining()
+    {
+        _program.Exercises.Add("Приседания с собственным весом 10 раз");
+    }
+
+    public void AddStretching()
+    {
+        _program.Exercises.Add("Растяжка ног 5 минут");
+    }
+
+    public void AddYoga()
+    {
+        _program.Exercises.Add("Легкая йога 10 минут");
+    }
+
+    public FitnessProgram GetProgram()
+    {
+        return _program;
+    }
+}
+
+public class IntermediateWorkoutBuilder : IWorkoutBuilder
+{
+    private readonly FitnessProgram _program = new FitnessProgram { DifficultyLevel = "Средний" };
+
+    public void AddCardio()
+    {
+        _program.Exercises.Add("Бег 10 минут");
+    }
+
+    public void AddStrengthTraining()
+    {
+        _program.Exercises.Add("Отжимания 15 раз, Приседания с гантелями 15 раз");
+    }
+
+    public void AddStretching()
+    {
+        _program.Exercises.Add("Динамическая растяжка 10 минут");
+    }
+
+    public void AddYoga()
+    {
+        _program.Exercises.Add("Хатха-йога 15 минут");
+    }
+
+    public FitnessProgram GetProgram()
+    {
+        return _program;
+    }
+}
+
+public class AdvancedWorkoutBuilder : IWorkoutBuilder
+{
+    private readonly FitnessProgram _program = new FitnessProgram { DifficultyLevel = "Продвинутый" };
+
+    public void AddCardio()
+    {
+        _program.Exercises.Add("Интервальный бег 20 минут");
+    }
+
+    public void AddStrengthTraining()
+    {
+        _program.Exercises.Add("Приседания со штангой 20 раз, Отжимания на брусьях 15 раз");
+    }
+
+    public void AddStretching()
+    {
+        _program.Exercises.Add("Глубокая растяжка 15 минут");
+    }
+
+    public void AddYoga()
+    {
+        _program.Exercises.Add("Аштанга-йога 20 минут");
+    }
+
+    public FitnessProgram GetProgram()
+    {
+        return _program;
+    }
+}
+
+
+public class WorkoutDirector
+{
+    private readonly IWorkoutBuilder _builder;
+
+    public WorkoutDirector(IWorkoutBuilder builder)
+    {
+        _builder = builder;
+    }
+
+    public void ConstructWorkout()
+    {
+        _builder.AddCardio();
+        _builder.AddStrengthTraining();
+        _builder.AddStretching();
+        _builder.AddYoga();
+    }
+
+    public FitnessProgram GetWorkout()
+    {
+        return _builder.GetProgram();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var beginnerDirector = new WorkoutDirector(new BeginnerWorkoutBuilder());
+        beginnerDirector.ConstructWorkout();
+        var beginnerWorkout = beginnerDirector.GetWorkout();
+        beginnerWorkout.ShowProgram();
+
+        var intermediateDirector = new WorkoutDirector(new IntermediateWorkoutBuilder());
+        intermediateDirector.ConstructWorkout();
+        var intermediateWorkout = intermediateDirector.GetWorkout();
+        intermediateWorkout.ShowProgram();
+
+        var advancedDirector = new WorkoutDirector(new AdvancedWorkoutBuilder());
+        advancedDirector.ConstructWorkout();
+        var advancedWorkout = advancedDirector.GetWorkout();
+        advancedWorkout.ShowProgram();
+
+        Console.ReadKey();
+    }
+}
